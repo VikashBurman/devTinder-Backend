2cc7de9d54ee762a4aa2d065c24d7022919728fc
Vikash Burman <vikasburman37@gmail.com>
2025-02-04 22:42:38 +0530

    User saved in db

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "Virat@gmail.com",
+    password: "virat@123",
+    age: 20,
+    gender: "male",
+  });
+
+  try {
+    await user.save();
+    res.send("User Saved Successfully");
+  } catch (error) {
+    res.status(400).send("Error in saving user")
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("homepage");
@@ -14,12 +33,6 @@ connectDB()
       console.log("Server Started at 4000");
     });
   })
-  .catch((err) => { 
+  .catch((err) => {
     console.error("Not Connected");
   });
-
-
-
-
-
-  
