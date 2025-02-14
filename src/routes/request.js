72342d25340fbe86ed9f6bd0d9f6e32b39430f02
Vikash Burman <vikasburman37@gmail.com>
2025-02-14 17:25:43 +0530

    Sender/Receiver validation added

--- src/routes/request.js
+++ src/routes/request.js
@@ -7,22 +7,40 @@ const requestRouter = express.Router();
 requestRouter.post(
   "/request/send/:status/:receiverUserId",
   userAuth,
-  async(req, res) => {
+  async (req, res) => {
     try {
       const status = req.params.status;
       const receiverUserId = req.params.receiverUserId;
       const senderUserId = req.user._id;
 
+      const allowedMethod = ["interested", "ignored"];
+      if (!allowedMethod.includes(status)) {
+        return res.status(400).json({ message: "Invalid action " + status });
+      }
+
+      const existingConnectionReq = await ConnectionRequestModel.findOne({
+        $or: [
+          //sender send request to reciever but sender cant send again
+          { senderUserId,receiverUserId },
+          //opposite once sender send request receiver cant send same
+          { senderUserId: receiverUserId, receiverUserId: senderUserId },
+        ],
+      });
+
+      if(existingConnectionReq){
+        return res.status(400).json({message:"Connection Request Already Exist!!"});
+      }
+
       const connectionRequest = new ConnectionRequestModel({
         senderUserId,
         receiverUserId,
-        status
+        status,
       });
       const data = await connectionRequest.save();
 
-      res.json({message:"Request Sent Successfully",data});
+      res.json({ message: "Request Sent Successfully", data });
     } catch (error) {
-      res.status(400).send("Error: "+error)
+      res.status(400).send("Error: " + error);
     }
   }
 );
